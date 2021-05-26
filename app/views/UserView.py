2bb96f56d5bc5369aa7dd31fff8cdfb0de7abd96
Sanif <sanifss@gmail.com>
2021-05-27 01:50:59 +0530

    minor fix in uervuew

--- app/views/UserView.py
+++ app/views/UserView.py
@@ -48,7 +48,7 @@ def create():
 @auth.auth_required
 def get_all():
     users = UserModel.get_all_users()
-    ser_users = user_schema.dump(users, many=True).data
+    ser_users = user_schema.dump(users, many=True)
     return custom_response(ser_users, 200)
 
 
